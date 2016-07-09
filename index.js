eb4882149be1a145e7fe88768d4518da1c7f1565
steelbrain <de.steelbrain@gmail.com>
2016-07-09 03:05:53 -0700

    :new: Add new config

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var path = require('path')
-var fs = require('fs')
-var filename = path.join(__dirname, '.eslintrc')
-var data = fs.readFileSync(filename, {encoding: 'utf-8'})
-module.exports = JSON.parse(data)
+/* @flow */
+
+module.exports = {
+  'parser': require.resolve('babel-eslint'),
+  'extends': require.resolve('eslint-config-airbnb-base'),
+  'rules': {
+    'semi': [2, 'never'],
+    'max-len': [1, 250],
+    'no-console': 0,
+    'comma-dangle': 2,
+    'prefer-template': 0,
+    'import/no-unresolved': 0,
+    'prefer-arrow-callback': 0,
+    'generator-star-spacing': 0,
+  },
+  'globals': {
+    'atom': true,
+  }
+}
