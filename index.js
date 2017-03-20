2802c9c6cb58178c476ad27f911e03a594f424d9
Steel Brain <i@steelbrain.me>
2017-03-21 01:27:36 +0500

    :art: Self lint

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 /* @flow */
 
 module.exports = {
-  'parser': require.resolve('babel-eslint'),
-  'extends': require.resolve('eslint-config-airbnb-base'),
+  parser: require.resolve('babel-eslint'),
+  extends: require.resolve('eslint-config-airbnb'),
   'rules': {
-    'semi': ["error", 'never'],
-    'max-len': ["warning", 250],
-    'func-names': "off",
-    'no-console': "off",
-    'comma-dangle': "error",
-    'prefer-template': "off",
-    'import/no-unresolved': "off",
+    'semi': ['error', 'never'],
+    'max-len': ['warn', 250],
+    'func-names': 'off',
+    'no-console': 'off',
+    'comma-dangle': 'error',
+    'prefer-template': 'off',
+    'import/no-unresolved': 'off',
     'no-restricted-syntax': [
-      "error",
+      'error',
       'DebuggerStatement',
       'LabeledStatement',
       'WithStatement',
     ],
-    'no-plusplus': "off",
-    'no-continue': "off",
-    'prefer-arrow-callback': "off",
-    'class-methods-use-this': "off",
-    'generator-star-spacing': "off",
-    'space-before-function-paren': "off",
-    'import/no-extraneous-dependencies': "off"
+    'no-plusplus': 'off',
+    'no-continue': 'off',
+    'prefer-arrow-callback': 'off',
+    'class-methods-use-this': 'off',
+    'generator-star-spacing': 'off',
+    'space-before-function-paren': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
   'globals': {
     'atom': true,
@@ -31,5 +31,5 @@ module.exports = {
   'env': {
     'node': true,
     'browser': true,
-  }
+  },
 }
