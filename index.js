2eb50d5c3ff3608dcabb325abd70aafe1e79eaa3
Steel Brain <i@steelbrain.me>
2017-10-01 16:05:33 -0700

    :art: Fine tune rules a bit

--- index.js
+++ index.js
@@ -9,27 +9,14 @@ module.exports = {
     'func-names': 'off',
     'no-console': 'off',
     'comma-dangle': 'error',
-    'prefer-template': 'off',
-    'import/no-unresolved': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'DebuggerStatement',
-      'LabeledStatement',
-      'WithStatement',
-    ],
     'no-plusplus': 'off',
     'no-continue': 'off',
     'prefer-arrow-callback': 'off',
     'class-methods-use-this': 'off',
     'generator-star-spacing': 'off',
     'space-before-function-paren': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/extensions': 'off',
-    // ^ This is required for unresolved imports like "atom"
-    'react/sort-comp': 'off',
     'react/jsx-filename-extension': 'off',
     'jsx-a11y/href-no-hash': 'off',
-    'no-await-in-loop': 'off'
   },
   'globals': {
     'atom': true,
