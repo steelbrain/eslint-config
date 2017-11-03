b2a2e13be96f807847cb967f83276a5da58175d9
Steel Brain <i@steelbrain.me>
2017-11-03 05:35:58 -0700

    :new: Allow for-of statements

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ module.exports = {
     'space-before-function-paren': 'off',
     'react/jsx-filename-extension': 'off',
     'jsx-a11y/href-no-hash': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
+      },
+    ],
   },
   globals: {
     atom: true,
